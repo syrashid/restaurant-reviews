d2000299abf3e0116335283cb79cd718b77e03a7
Sy Rashid <sy.rashid.11@gmail.com>
2020-11-18 03:08:48 +0100

    FINISH LECTURE

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,9 +1,32 @@
 class ReviewsController < ApplicationController
   def new
-
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
   end
 
   def create
+    @review = Review.new(review_params)
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+    raise
 
+    if @review.save
+      redirect_to restaurant_path(@restaurant), notice: 'Review was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @restaurant = @review.restaurant
+    @review.destroy
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
     end
     # get 'chef', to: 'restaurants#chef'
   end
+  resources :reviews, only: :destroy
 end
